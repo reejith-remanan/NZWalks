b655de922628cd2e7c0f6973d84debc2188b0309
reejith-remanan <reejithremanan@gmail.com>
2025-02-06 08:12:05 +0530

    changed all functions to Async and implemented repository patten

---
+++ NZWalks.API/Repositories/InMemoryRegioRepository.cs
@@ -0,0 +1,20 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    //public class InMemoryRegionRepository : IRegionRepository
+    //{
+    //    public Task<List<Region>> GetAllAsync()
+    //    {
+    //        return Task.FromResult(new List<Region>
+    //    {
+    //        new Region()
+    //        {
+    //            Id = Guid.NewGuid(),
+    //            Code = "RR",
+    //            Name = "Random Region"
+    //        }
+    //    });
+    //    }
+    //}
+}
